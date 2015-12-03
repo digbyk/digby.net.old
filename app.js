3c0610d0d271975f17d46d574e00cd5d7de840a3
digbyk <digby@digby.net>
2015-12-03 23:00:10 +0000

    Removing Loggly again

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
 var newrelic = require('newrelic');
 require('dotenv').load();
 
-var winston = require('winston');
-require('winston-loggly');
-winston.add(winston.transports.Loggly, {
-  token: "14d0eea3-dbce-4040-acd3-9e1ec59e500c",
-  subdomain: "digbyk",
-  tags: ["digbynet"],
-  json: true
-});
-
 var express = require('express');
 var app = express();
 
@@ -31,5 +22,5 @@ app.set('port', (process.env.PORT || 3000));
 app.use(express.static(__dirname + '/public'));
 
 app.listen(app.get('port'), function () {
-  winston.log("Node app is running at localhost:" + app.get('port'))
+  console.log('Node app is running at localhost:' + app.get('port'))
 });
