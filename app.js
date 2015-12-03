7c2aa991d63bc04fa6d342dd247198342a2a0872
digbyk <digby@digby.net>
2015-12-03 21:07:19 +0000

    Adding Winston and Loggly

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 var newrelic = require('newrelic');
+require('dotenv').load();
+
+var winston = require('winston');
+require('winston-loggly');
+winston.add(winston.transports.Loggly, {
+  token: "14d0eea3-dbce-4040-acd3-9e1ec59e500c",
+  subdomain: "digbyk",
+  tags: ["digbynet"],
+  json: true
+});
+
 var express = require('express');
 var app = express();
 
-app.locals.newrelic = newrelic;
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var RedisStore = require('connect-redis')(session);
+
+app.use(session({
+  store: new RedisStore({ url: process.env.REDIS_URL }),
+  secret: 'scrt',
+  resave: true,
+  saveUninitialized: true
+}));
 
 app.set('port', (process.env.PORT || 3000));
 app.use(express.static(__dirname + '/public'));
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+app.listen(app.get('port'), function () {
+  winston.log("Node app is running at localhost:" + app.get('port'))
 });
