699e6674a97b19d3a191297229ca2087b2ba824d
digbyk <digby@digby.net>
2015-12-04 18:27:12 +0000

    Converting main site to Jade

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var newrelic = require('newrelic');
 require('dotenv').load();
 
+var path = require('path');
 var express = require('express');
 var app = express();
 
@@ -12,15 +13,21 @@ var session = require('express-session');
 var RedisStore = require('connect-redis')(session);
 
 app.use(session({
-  store: new RedisStore({ url: process.env.REDIS_URL }),
-  secret: 'scrt',
-  resave: true,
-  saveUninitialized: true
+	store: new RedisStore({ url: process.env.REDIS_URL }),
+	secret: 'scrt',
+	resave: true,
+	saveUninitialized: true
 }));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+var routes = require('./routes');
+app.use('/', routes);
 
 app.set('port', (process.env.PORT || 3000));
 app.use(express.static(__dirname + '/public'));
 
 app.listen(app.get('port'), function () {
-  console.log('Node app is running at localhost:' + app.get('port'))
+	console.log('Node app is running at localhost:' + app.get('port'))
 });
