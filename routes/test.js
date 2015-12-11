4e397e1150d8272e8d89b01b597bca46733fc112
digbyk <digby@digby.net>
2015-12-11 17:28:09 +0000

    test

--- routes/test.js
+++ routes/test.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var redis = require("redis");
-var client = redis.createClient();
+//var redis = require("redis");
+//var client = redis.createClient();
 
 router.get('/', function (req, res) {
 	res.render('test', {user: req.user});
