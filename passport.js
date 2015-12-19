72c9b21d4f8d399af721b42e5c98aa2ee042eb82
digbyk <digby@digby.net>
2015-12-19 08:14:37 +0000

    Tidying login

--- passport.js
+++ passport.js
@@ -1,6 +1,5 @@
 var passport = require('passport');
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-var stripe = require('stripe')(process.env.STRIPE_KEY);
 
 var User = require('./model/user.js');
 
@@ -12,33 +11,30 @@ passport.deserializeUser(function (obj, done) {
 	done(null, obj);
 });
 
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CLIENT_CALLBACK,
-	prompt: 'none',
-	accessType: 'online',
-	approvalPrompt: 'auto'
-},
+passport.use(new GoogleStrategy(
+	{
+		clientID: process.env.GOOGLE_CLIENT_ID,
+		clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+		callbackURL: process.env.GOOGLE_CLIENT_CALLBACK,
+		prompt: 'none',
+		accessType: 'online',
+		approvalPrompt: 'auto'
+	},
 	function (accessToken, refreshToken, profile, done) {
 		process.nextTick(function () {
-			User.findOne({ email: profile.emails[0].value })
+			User.findOneAndUpdate(
+				{ email: profile.emails[0].value },
+				{
+					email: profile.emails[0].value,
+					displayName: profile.displayName,
+					photoUrl: profile.photos[0].value,
+					accessToken: accessToken,
+					lastLoggedIn: new Date()
+				},
+				{ upsert: true, 'new': true }
+				)
 				.then(function (user) {
-					if (!user) {
-						user = new User({
-							email: profile.emails[0].value,
-							displayName: profile.displayName,
-							photoUrl: profile.photos[0].value
-						});
-					}
-					user.accessToken = accessToken;
-					user.lastLoggedIn = new Date();
-					return user.save();
-				}).then(function (user) {
-					stripe.customers.create({ email: user.email }, function (err, result) {
-						if (err) console.error(err);
-						done(null, user);
-					});
+					done(null, user);
 				}).catch(function (err) {
 					console.error(err);
 					done(err);
