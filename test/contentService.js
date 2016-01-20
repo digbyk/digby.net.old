c56a4d1061bef875a275b4078cddc71c43402c09
digbyk <digby@digby.net>
2016-01-20 07:41:34 +0000

    Moving services around

---
+++ test/contentService.js
@@ -0,0 +1,9 @@
+var contentService = require('../lib/contentService.js');
+
+describe('ContentService tests', function () {
+  describe('#get()', function () {
+    it('cs should do stuff', function (done) {
+        done();
+    });
+  });
+});
