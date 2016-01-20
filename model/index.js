37ce1d76d167dee3ba77fc7c09757e27d37e78f2
digbyk <digby@digby.net>
2016-01-20 07:41:02 +0000

    Adding Winston logging

--- model/index.js
+++ model/index.js
@@ -1,34 +1,37 @@
 var url = process.env.MONGO_URL;
 
+var winston = require('../lib/logging.js');
+
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 mongoose.connect(url, {
-  server: {
-    auto_reconnect: true
-  }
+    server: {
+        auto_reconnect: true
+    }
 });
 
 mongoose.connection.on('connected', function () {
-  console.info('Mongoose connected');
+    winston.log('info', 'Mongoose connected');
+    console.info('Mongoose connected');
 });
 
 mongoose.connection.on('error', function (err) {
-  console.info('Mongoose connection error: ' + err);
-  mongoose.connect(url, {
-    server: {
-      auto_reconnect: true
-    }
-  });
+    console.info('Mongoose connection error: ' + err);
+    mongoose.connect(url, {
+        server: {
+            auto_reconnect: true
+        }
+    });
 });
 
 mongoose.connection.on('disconnected', function () {
-  console.info('Mongoose disconnected');
+    console.info('Mongoose disconnected');
 });
 
 process.on('SIGINT', function () {
-  mongoose.connection.close(function () {
-    console.info('Mongoose disconnected through app termination');
-    process.exit(0);
-  });
+    mongoose.connection.close(function () {
+        console.info('Mongoose disconnected through app termination');
+        process.exit(0);
+    });
 });
