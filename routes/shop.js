ec497cf03a7e1930d4ba08cc3afd28cb41ceb8f6
digbyk <digby@digby.net>
2016-02-19 21:25:17 +0000

    Using arrow functions now

--- routes/shop.js
+++ routes/shop.js
@@ -1,3 +1,5 @@
+'user strict';
+
 var express = require('express');
 var router = express.Router();
 
@@ -10,34 +12,33 @@ var auth0 = new ManagementClient({
 	domain: process.env.AUTH0_DOMAIN
 });
 
-router.use(function isAuthenticated(req, res, next) {
+router.use((req, res, next) => {
 	if (req.isAuthenticated()) {
 		next();
 	} else {
-		//req.flash('error', 'You must be logged in to do that.')
-		//next();
+		req.flash('error', 'You must be logged in to do that.')
 		res.redirect('/');
 	}
 });
 
-router.use(function checkStripeAccount(req, res, next) {
+router.use((req, res, next) => {
 	logger.debug('Creating customer');
 	auth0.users.get({ id: req.user.user_id })
-		.then(function (user) {
+		.then(user => {
 			if (typeof user.app_metadata.customerId != 'undefined') throw new Error('Customer already exists');
 			logger.debug(user.app_metadata);
 			return stripe.customers.create({ email: user.email });
 		})
-		.then(function (customer) {
+		.then(customer => {
 			logger.debug(customer.id);
 			return auth0.users.update({ id: req.user.user_id }, { 'app_metadata': { 'customerId': customer.id } });
 		})
-		.then(function (user) {
+		.then(user => {
 			logger.debug(user.displayName);
 			logger.debug(user.email);
 			logger.debug(user.customerId);
 			next();
-		}).catch(function (err) {
+		}).catch(err => {
 			if (err.message != 'Customer already exists') {
 				logger.debug(err);
 				res.redirect('/');
@@ -46,16 +47,16 @@ router.use(function checkStripeAccount(req, res, next) {
 		});
 });
 
-router.get('/', function (req, res) {
-	stripe.products.list().then(function (products) {
+router.get('/', (req, res) => {
+	stripe.products.list().then(products => {
 		res.render('shop/index', { products: products });
-	}).catch(function (err) {
+	}).catch(err => {
 		logger.log(err);
 		res.status(500);
 	});
 });
 
-router.get('/basket', function (req, res) {
+router.get('/basket', (req, res) => {
 	stripe.customers.retrieve(req.user.app_metadata.customerId)
 		.then(function (customer) {
 			logger.log(customer);
@@ -66,7 +67,7 @@ router.get('/basket', function (req, res) {
 		});
 });
 
-router.post('/charge', function (req, res) {
+router.post('/charge', (req, res) => {
 	logger.log(req.user.app_metadata.customerId);
 	stripe.charges.create({
 		amount: 1000, // amount in cents, again
@@ -74,19 +75,19 @@ router.post('/charge', function (req, res) {
 		customer: req.user.app_metadata.customerId,
 		//source: req.body.stripeToken,
 		description: 'Example charge'
-	}).then(function (charge) {
+	}).then(charge => {
 		logger.log(charge);
 		res.redirect('/shop/basket');
-	}).catch(function (err) {
+	}).catch(err => {
 		logger.log(err);
 		res.status(500);
 	});
 });
 
-router.get('/:productId', function (req, res) {
+router.get('/:productId', (req, res) => {
 	stripe.products.retrieve(req.params.productId).then(function (product) {
 		res.render('shop/product', { product: product });
-	}).catch(function (err) {
+	}).catch(err => {
 		logger.log(err);
 		res.status(500);
 	});
