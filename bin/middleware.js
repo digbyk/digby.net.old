86fef4f4520a2b218230aa9ceb99d3ac01019b75
digbyk <digby@digby.net>
2016-02-20 11:28:55 +0000

    Starting to get tests working

---
+++ bin/middleware.js
@@ -0,0 +1,20 @@
+var assert = require('assert');
+var httpMocks = require('node-mocks-http');
+
+var mw = require('../lib/middleware');
+
+describe('Middleware tests', function () {
+	describe('#get()', function () {
+		it('mw should do stuff', function (done) {
+			var request = httpMocks.createRequest();
+			var response = httpMocks.createResponse();
+
+			assert.notEqual(mw, null);
+			mw(request, response, function () {
+				assert.equal(response.test, 'test');
+				assert.notEqual(response, null);
+				done();
+			});
+		});
+	});
+});
