e6f03a77902b2b22ac32b14eb9f0a7125066f9f6
digbyk <digby@digby.net>
2016-02-25 07:50:45 +0000

    AIWF stuff

---
+++ test/awif-service.js
@@ -0,0 +1,9 @@
+var aiwfService = require('../lib/aiwf-service.js');
+
+describe('AIWF tests', function () {
+	describe('#get()', function () {
+		it('cs should do stuff', function (done) {
+			done();
+		});
+	});
+});
--- test/contentService.js
+++
@@ -1,9 +0,0 @@
-//var contentService = require('../lib/contentService.js');
-
-describe('ContentService tests', function () {
-  describe('#get()', function () {
-    it('cs should do stuff', function (done) {
-        done();
-    });
-  });
-});
