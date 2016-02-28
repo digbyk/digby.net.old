5baa3f92a4280819871ad6854df0ea67609c4a04
digbyk <digby@digby.net>
2016-02-28 18:22:26 +0000

    Removing unnecessary code

--- routes/aiwf.js
+++ routes/aiwf.js
@@ -6,7 +6,6 @@ var Gift = require('../model/aiwf.js').Gift;
 
 var express = require('express');
 var router = express.Router();
-var passport = require('passport');
 
 var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn();
 
