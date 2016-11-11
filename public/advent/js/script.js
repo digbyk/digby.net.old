4801bea2b24c94ea459037e73f5d9d4b55ea7d7a
digbyk <digby@digby.net>
2016-11-11 21:53:10 +0000

    2016 advent changes

--- public/advent/js/script.js
+++ public/advent/js/script.js
@@ -5,99 +5,99 @@ $(document).ready(function () {
         this.windows = [
             {
                 id: 1,
-                url: "https://lh3.googleusercontent.com/-2aI0oUWktjM/VlrtujF5LAI/AAAAAAAAUfs/G6pS60ehB9I/s1024-Ic42/IMAG0415.jpg"
+                url: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/1909934_10153150520517105_7720275046632008327_n.jpg?oh=595b83c657a6ed0e9b6f04eb6bf329dd&oe=58D2D01E'
             },
             {
                 id: 2,
-                url: "https://scontent-lhr3-1.xx.fbcdn.net/hphotos-xft1/t31.0-8/12291240_10153091966570303_8914216623113000082_o.jpg"
+                url: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/14523125_10153818792017105_3486422491110618457_n.jpg?oh=12dc9aed1c1ce12e31168c504f08beaf&oe=5895E906'
             },
             {
                 id: 3,
-                url: "https://scontent-lhr3-1.xx.fbcdn.net/hphotos-xpa1/v/t1.0-9/12227697_10153084786485303_7842298098934043258_n.jpg?oh=7c790069e6e5bc8dfdb36b9001dc7a9b&oe=56D861E9"
+                url: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/13920990_10153607908885303_4640550007642409362_n.jpg?oh=b7290fa9b8863438f63fe4e290b7980e&oe=588E9F76'
             },
             {
                 id: 4,
-                url: "https://scontent-lhr3-1.xx.fbcdn.net/hphotos-xpf1/v/t1.0-9/74040_458888935302_7326877_n.jpg?oh=88c6a45eda2797bee2d2726abd923db7&oe=56DEA1CF"
+                url: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/14433117_10153707513442105_329803376925509983_n.jpg?oh=1648de322962f5a477f16b529a43cd2c&oe=58C89164'
             },
             {
                 id: 5,
-                url: "https://scontent-lhr3-1.xx.fbcdn.net/hphotos-xta1/v/t1.0-9/11223487_10152963386170303_3244072003834948523_n.jpg?oh=335cca62d7f2e62a3886c5570d158cdd&oe=56F2CAEB"
+                url: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/12472472_10153338497275303_6069754433110048508_n.jpg?oh=2321f8a49eda6d3ff6b53d69e93f3c2b&oe=588CED11'
             },
             {
                 id: 6,
-                url: "https://scontent-lhr3-1.xx.fbcdn.net/hphotos-xfa1/v/t1.0-9/10174910_964353173582_3163681278325573464_n.jpg?oh=6da02b1c1de3a4961a0542b3adfe953b&oe=56E5AE75"
+                url: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/14650741_10153796957610303_3682037351199585503_n.jpg?oh=39d47484a1dbc011dbdec5453b588e8b&oe=588727A0'
             },
             {
                 id: 7,
-                url: "https://scontent-lhr3-1.xx.fbcdn.net/hphotos-xaf1/v/t1.0-9/10406550_10152703764020303_687024180026650706_n.jpg?oh=6616cf0714cd877256c9285ec19dd09a&oe=56F016DD"
+                url: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/14680729_10153796752675303_3751494604359728131_n.jpg?oh=5d9d5444136ad8610fbb0781e1d2572f&oe=58C95EAF'
             },
             {
                 id: 8,
-                url: "https://scontent-lhr3-1.xx.fbcdn.net/hphotos-xfa1/v/t1.0-9/10169314_10151981344151373_4703763617593741382_n.jpg?oh=91caf72433ca5d6bb05819cd9f266ba0&oe=56F7F12B"
+                url: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/10952123_10152586022447105_1025918826505707226_n.jpg?oh=575489e2a67145f5e46c9c20be81d1e3&oe=58962B40'
             },
             {
                 id: 9,
-                url: "https://scontent-lhr3-1.xx.fbcdn.net/hphotos-prn2/t31.0-8/1921137_10151880872477105_445731357_o.jpg"
+                url: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/14470537_10153743875395303_2753648594172511370_n.jpg?oh=7165b6618d6ff24348f352e7a0245854&oe=58C86081'
             },
             {
                 id: 10,
-                url: "https://scontent-lhr3-1.xx.fbcdn.net/hphotos-xaf1/v/t1.0-9/602976_808247780042_1470757111_n.jpg?oh=a1a3eeeeea1bcd87a287b248806dcb69&oe=56FA3B9A"
+                url: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/14519924_10153743875330303_4971513066101637792_n.jpg?oh=5a0a2634cd5a237f7079bea35e18860b&oe=588AC251'
             },
             {
                 id: 11,
-                url: "https://scontent-lhr3-1.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/386122_10150381466880303_1223271224_n.jpg?oh=727c7d57d813d25ecb2227217430c4ce&oe=56EBBF8B"
+                url: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/14333203_10153699987905303_5997649135490091015_n.jpg?oh=299440d6db2acbbc1c91860b07ba2fb7&oe=58C2D5AA'
             },
             {
                 id: 12,
-                url: "https://scontent-lhr3-1.xx.fbcdn.net/hphotos-ash2/v/t1.0-9/943270_10151464972772105_1610934705_n.jpg?oh=6820cb92649544e2cb8ffe8070fe48ab&oe=56E2C773"
+                url: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/14222323_10153672224975303_2856983273350035368_n.jpg?oh=113b471350ed3262e1c3afc753873726&oe=58CE96CD'
             },
             {
                 id: 13,
-                url: "https://scontent-lhr3-1.xx.fbcdn.net/hphotos-xpa1/v/t1.0-9/577700_10151018608066558_661654415_n.jpg?oh=4575c7e2410e4ad6d35b619fe798fd9e&oe=56F7EAAE"
+                url: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/14100350_10153642690415303_1518184476410929954_n.jpg?oh=3744b2fa1c254e5566e65659aa11dec5&oe=58D54705'
             },
             {
                 id: 14,
-                url: "https://scontent-lhr3-1.xx.fbcdn.net/hphotos-xaf1/t31.0-8/s960x960/328185_10151007884305303_459458230_o.jpg"
+                url: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/13962671_10153640006915303_5073336400432818745_n.jpg?oh=d1b9fd5bc438fa66a1c96718f40ceddf&oe=58C33D76'
             },
             {
                 id: 15,
-                url: "https://scontent-lhr3-1.xx.fbcdn.net/hphotos-xfa1/v/t1.0-9/11902485_10152963385760303_1057592919052719064_n.jpg?oh=0af5c0ccc399d09c7babb79dec8a229f&oe=56E6849D"
+                url: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/14034858_10153640006770303_502771901378645985_n.jpg?oh=be98366d59f3474efa0646a9bcd6fec7&oe=5893BB55'
             },
             {
                 id: 16,
-                url: "https://scontent-lhr3-1.xx.fbcdn.net/hphotos-xfa1/v/t1.0-9/11800079_10152900074625303_9117974804956416280_n.jpg?oh=3c8276f9d88d2fa97b50620fa41fd1fb&oe=56EBA3F4"
+                url: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/13680698_10153560767370303_1074547845979499295_n.jpg?oh=61e5db7a5b321d0c2a0d0ee9369c4e87&oe=58CE4267'
             },
             {
                 id: 17,
-                url: "https://scontent-lhr3-1.xx.fbcdn.net/hphotos-xtp1/v/t1.0-9/11390159_10152787210440303_500717380551583677_n.jpg?oh=fa32968afa4db474db9b896bbe051046&oe=56E36091"
+                url: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/13310464_10153463850675303_2359812831567832337_n.jpg?oh=f6a6e9fca598a27fc9d6fb9c26ccd864&oe=5895AB33'
             },
             {
                 id: 18,
-                url: "https://scontent-lhr3-1.xx.fbcdn.net/hphotos-xpa1/v/t1.0-9/10437437_10152128495100303_2060949527398144035_n.jpg?oh=eae1d8714d63cdfc3f90a30ab0b6c1b3&oe=56FA3402"
+                url: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/12928314_10153338497375303_7233519773473609851_n.jpg?oh=03ffac0280967489afeef13e0c285afc&oe=58C9B282'
             },
             {
                 id: 19,
-                url: "https://scontent-lhr3-1.xx.fbcdn.net/hphotos-frc3/t31.0-8/1048770_10151464969627105_287231828_o.jpg"
+                url: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/12804786_10153279223840303_3756054469283268983_n.jpg?oh=9435a439ec7e5663212b3395473cbbe6&oe=588BF8E6'
             },
             {
                 id: 20,
-                url: "https://scontent-lhr3-1.xx.fbcdn.net/hphotos-xpf1/v/t1.0-0/p206x206/968792_10151464976762105_350108743_n.jpg?oh=50b33744a16b6833f206c16651b9dd8b&oe=56D71A6F"
+                url: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/12644799_10153207872730303_5601908276513821475_n.jpg?oh=97c5d6c1b848804e0c5d394417eda854&oe=588DECFB'
             },
             {
                 id: 21,
-                url: "https://scontent-lhr3-1.xx.fbcdn.net/hphotos-frc3/v/t1.0-9/10710546_958461171202_5975254869642640944_n.jpg?oh=6dddb90d8b069cec188e4d2ecbd864ee&oe=56F9105E"
+                url: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/5802_10153160153665303_7759409237880033875_n.jpg?oh=3a896de78c23c1717ca949bb099a42b2&oe=58D1451C'
             },
             {
                 id: 22,
-                url: "https://scontent-lhr3-1.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/532942_10152472353620303_1462947632884721570_n.jpg?oh=ae59e5d0f162ac4e9624b77cba843cc3&oe=56DC80CA"
+                url: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/7786_10153149895620303_1598460986081281511_n.jpg?oh=71f5a2bc6d9f4fb149cbd91df16b8df3&oe=58C56BED'
             },
             {
                 id: 23,
-                url: "https://scontent-lhr3-1.xx.fbcdn.net/hphotos-xpt1/v/t1.0-9/12196125_10153062269350303_3170830031699105814_n.jpg?oh=28f5d7d9c73296ac068433c13581d86d&oe=56E7B7E6"
+                url: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/10457780_10153144920505303_7019166592378217242_n.jpg?oh=80814ef93f1ad32c936a02976bfcffbb&oe=5894C426'
             },
             {
                 id: 24,
-                url: "https://scontent-lhr3-1.xx.fbcdn.net/hphotos-xfp1/t31.0-8/11953590_1659351911018619_7156952904029532760_o.jpg"
+                url: 'https://scontent-lhr3-1.xx.fbcdn.net/t31.0-8/14691341_10153788788077105_1020378609311285391_o.jpg'
             }
         ]
     };
@@ -111,41 +111,41 @@ $(document).ready(function () {
         $.jStorage.set('advent', { openWindows: {} });
     });
 
-	setInterval(function () {
-		updateWindows();
-	}, 5 * 60 * 1000);
+    setInterval(function () {
+        updateWindows();
+    }, 5 * 60 * 1000);
 
-	function updateWindows() {
-		$('.window').each(function () {
-			var thumbnail = $(this).data('thumbnail');
-			var date = new Date();
-			var day = date.getDate();
-			var month = date.getMonth();
-			var windowDay = $(this).data('day');
-			var advent = $.jStorage.get('advent');
-			if (advent == null) advent = { openWindows: {} };
-			$.jStorage.set('advent', advent);
-			$(this).css('order', (Math.floor(100 * Math.random())).toString());
-			if ((day >= windowDay && month == 11) || document.URL.indexOf('debug') > -1) {
-				$(this).addClass('openable', 'true');
-				if (advent.openWindows['day' + windowDay] == true) {
-					$(this).addClass('opened', 'true');
-					$(this).css('background-image', 'url(' + thumbnail + ')');
-					$(this).featherlight($(this).data('content'), {});
-				}
-				$(this).click(function () {
-					var ding = document.getElementById('ding');
-					ding.play();
-					ding.volume = 0.1;
-					var advent = $.jStorage.get('advent');
-					$(this).addClass('opened', 'true');
-					advent.openWindows['day' + windowDay] = true;
-					$.jStorage.set('advent', advent);
-					$(this).css('background-image', 'url(' + thumbnail + ')');
-					$(this).featherlight($(this).data('content'), {});
-				});
-			}
-		});
+    function updateWindows() {
+        $('.window').each(function () {
+            var thumbnail = $(this).data('thumbnail');
+            var date = new Date();
+            var day = date.getDate();
+            var month = date.getMonth();
+            var windowDay = $(this).data('day');
+            var advent = $.jStorage.get('advent');
+            if (advent == null) advent = { openWindows: {} };
+            $.jStorage.set('advent', advent);
+            $(this).css('order', (Math.floor(100 * Math.random())).toString());
+            if ((day >= windowDay && month == 11) || document.URL.indexOf('debug') > -1) {
+                $(this).addClass('openable', 'true');
+                if (advent.openWindows['day' + windowDay] == true) {
+                    $(this).addClass('opened', 'true');
+                    $(this).css('background-image', 'url(' + thumbnail + ')');
+                    $(this).featherlight($(this).data('content'), {});
+                }
+                $(this).click(function () {
+                    var ding = document.getElementById('ding');
+                    ding.play();
+                    ding.volume = 0.1;
+                    var advent = $.jStorage.get('advent');
+                    $(this).addClass('opened', 'true');
+                    advent.openWindows['day' + windowDay] = true;
+                    $.jStorage.set('advent', advent);
+                    $(this).css('background-image', 'url(' + thumbnail + ')');
+                    $(this).featherlight($(this).data('content'), {});
+                });
+            }
+        });
 
-	}
+    }
 });
